4d737cf2a35d2d4adc7b528f08bce1c96cb7b729
AndrewChild <an.child@gmail.com>
2024-04-29 16:08:29 -0700

    Add timeline

--- GB_Book.py
+++ GB_Book.py
@@ -4,7 +4,7 @@ from dataStructure import *
 book = Book(
     name='The Garden',
     collaborators=['Andrew Child'],
-    repo='https://github.com/AndrewChild/The-Garden-Guidebook',
+    repo='https://andrewchild.github.io/The-Garden-Guidebook/',
     dl='https://github.com/AndrewChild/The-Garden-Guidebook/raw/main/guideBook.pdf',
     paths={
         'LaTeXTemplates': '../LocalBoulders/templates/',
